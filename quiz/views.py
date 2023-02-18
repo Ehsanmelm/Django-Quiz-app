5c6319310b0cf9d6e085925e1923a0187b22d818
ehsan <ehsanmelm82@gmail.com>
2023-02-18 16:52:18 +0330

    erasing additional comments

--- quiz/views.py
+++ quiz/views.py
@@ -32,35 +32,6 @@ class SiginView(View):
                 quiz_slug.save()
                 return HttpResponseRedirect(reverse('login_page'))
 
-            # try:
-            #     search_user = UserInfoModel.objects.get(
-            #         user_name=form.cleaned_data['user_name'])
-            #     msg = "user name has taken"
-            #     return render(request, "quiz/signin.html", {"form": form, "msg": msg})
-
-            # except:
-            #     quiz_slug = form.save(commit=False)
-            #     quiz_slug.slug = slugify(quiz_slug.email)
-            #     quiz_slug.save()
-            #     return HttpResponseRedirect(reverse('login_page'))
-
-        # if form.is_valid():
-        #     print(form.cleaned_data['user_name'])
-
-        #     # try:
-        #     #     search_user = UserInfoModel.objects.get(
-        #     #         user_name=form.cleaned_data['user_name'])
-        #     #     print(search_user.user_name)
-        #     #     # msg = "Username In Use!"
-        #     #     # return render(request, "quiz/signin.html", {"form": form, "msg": msg})
-        #     # except:
-        #     #     print("+++++++gggg")
-        #     #     quiz_slug = form.save(commit=False)
-        #     #     quiz_slug.slug = slugify(quiz_slug.email)
-        #     #     quiz_slug.save()
-
-        #     # form.save()
-        #     return HttpResponseRedirect(reverse('login_page'))
         return render(request, "quiz/signin.html", {"form": form})
 
 
@@ -69,18 +40,7 @@ class LoginView(View):
         return render(request, "quiz/login.html", {"is_sign_in": True})
 
     def post(self, request):
-        # try:
 
-        #     search_for_user = UserInfoModel.objects.get(
-        #         user_name=request.POST.get('name').lower())
-        #     print(request.POST.get('password'), search_for_user.password)
-        #     if search_for_user.password == request.POST.get('password'):
-        #         is_sign_in = True
-        #         return HttpResponseRedirect("quiz/"+search_for_user.slug)
-        #     else:
-        #         is_sign_in = False
-        #         msg = "User Doesn't Exist"
-        #         return render(request, "quiz/login.html", {"msg": msg, "user_model": search_for_user, "is_sign_in": is_sign_in})
         try:
             find_user = UserInfoModel.objects.get(user_name=request.POST.get(
                 'name'), password=request.POST.get('password'))
@@ -92,9 +52,6 @@ class LoginView(View):
             is_sign_in = False
             return render(request, "quiz/login.html", {"msg": msg, "is_sign_in ": is_sign_in})
 
-
-# def Quiz(request):
-#     passc
 
 class QuizView(View):
     def get(self, request, slug):
@@ -108,7 +65,6 @@ class QuizView(View):
         score = 0
         wrong_answer = 0
         num_of_questions = QuizModels.objects.all().count()
-        # print(num_of_questions)
         for i in range(len(questions)):
             chosen_option.append(request.POST.get(str(questions[i].id)))
         for loop_counter, i in enumerate(questions):
